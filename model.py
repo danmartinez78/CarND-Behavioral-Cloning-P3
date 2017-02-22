7ed3354b541d5b2aeef546d0b2213957fa74b993
Dan <danmartinez78@gmail.com>
2017-02-22 09:48:35 -0600

    good model

--- model.py
+++ model.py
@@ -7,8 +7,6 @@ from keras.preprocessing.image import ImageDataGenerator
 from keras.callbacks import ModelCheckpoint
 import pickle
 from sklearn.model_selection import train_test_split
-import cv2
-
 
 # import data
 with open('./data/data.p', mode='rb') as f:
@@ -18,30 +16,36 @@ with open('./data/data.p', mode='rb') as f:
 X_train, X_test, y_train, y_test = train_test_split(data['features'], data['labels'], test_size=0.2)
 
 # generators
-gen_train = ImageDataGenerator()
-gen_test = ImageDataGenerator()
+datagen = ImageDataGenerator()
+
+# compute quantities required for featurewise normalization
+# (std, mean, and principal components if ZCA whitening is applied)
+datagen.fit(X_train)
+
+testgen = ImageDataGenerator()
+
+# compute quantities required for featurewise normalization
+# (std, mean, and principal components if ZCA whitening is applied)
+testgen.fit(X_test)
 
 # model
 model = Sequential()
 # input layer w/ cropping
-model.add(Cropping2D(cropping=((20,10), (0,0)), input_shape=(160,320,3)))
-model.add(MaxPooling2D(pool_size=(5,5)))
-# normalize
+model.add(Cropping2D(cropping=((50,20), (0,0)), input_shape=(160,320,3)))
+# normalize to -0.5:0.5
 model.add(Lambda(lambda x: (x / 255.0) - 0.5))
-# 5x5 conv
-model.add(Convolution2D(3, 5, 5, border_mode='valid'))
-model.add(Activation('relu'))
 # 5x5 conv
 model.add(Convolution2D(24, 5, 5, border_mode='valid'))
 model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2,2)))
+# 5x5 conv
+model.add(Convolution2D(36, 5, 5, border_mode='valid'))
+model.add(Activation('relu'))
 # 5x5 conv
 model.add(Convolution2D(48, 5, 5, border_mode='valid'))
 model.add(Activation('relu'))
 # 3x3 conv
 model.add(Convolution2D(64, 3, 3, border_mode='valid'))
 model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2,2)))
 # 3x3 conv
 model.add(Convolution2D(64, 3, 3, border_mode='valid'))
 model.add(Activation('relu'))
@@ -50,11 +54,10 @@ model.add(Flatten())
 # fcn1
 model.add(Dense(100))
 model.add(Activation('relu'))
-model.add(Dropout(0.25))
+model.add(Dropout(0.50))
 # fcn2
 model.add(Dense(50))
 model.add(Activation('relu'))
-model.add(Dropout(0.25))
 # fcn3
 model.add(Dense(10))
 model.add(Activation('relu'))
@@ -64,14 +67,13 @@ model.add(Dense(1))
 # compile
 model.compile('adam', 'mse')
 check = ModelCheckpoint("./model.h5", verbose=1, save_best_only=True)
-#history = model.fit(X, y, batch_size=128, nb_epoch=2, validation_split=0.2)
-history = model.fit_generator(gen_train.flow(X_train, y_train, batch_size=64),
+history = model.fit_generator(datagen.flow(X_train, y_train, batch_size=128),
                 samples_per_epoch=len(X_train),
-                nb_epoch=5,
-                validation_data=gen_test.flow(X_test, y_test, batch_size=64),
+                nb_epoch=6,
+                validation_data=testgen.flow(X_test, y_test, batch_size=128),
                 nb_val_samples=len(X_train),
                 callbacks=[check],
-                verbose=2
+                verbose=1
                 )
 
 ### print the keys contained in the history object
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ steering_angles = []
 with open(csv_file, 'r') as f:
     reader = csv.reader(f)
     for row in reader:
-        correction = 0.5  # this is a parameter to tune
+        correction = 0.35  # this is a parameter to tune
         steering_center = float(row[3])
         steering_left = steering_center + correction
         steering_right = steering_center - correction
@@ -22,17 +22,9 @@ with open(csv_file, 'r') as f:
         img_left = np.asarray(Image.open(path + row[1].lstrip()))
         img_right = np.asarray(Image.open(path + row[2].lstrip()))
 
-        # augment with random brightness and skew
-        rows, cols, depth = img_center.shape
-        M = np.float32([[1, 0, np.random.randint(-50,50)], [0, 1, np.random.randint(-50,50)]])
-        image1 = cv2.warpAffine(img_center, M, (cols, rows))
-        image2 = img_center * np.random.random_sample()
-
         # add images and angles to data set
-        car_images.extend([img_center, img_left, img_right, np.fliplr(img_center), image2])
-        steering_angles.extend([steering_center, steering_left, steering_right, steering_center])
-
-
+        car_images.extend([img_center, img_left, img_right, np.fliplr(img_center), np.fliplr(img_left), np.fliplr(img_right)])
+        steering_angles.extend([steering_center, steering_left, steering_right, -steering_center, -steering_left, -steering_right])
 
 data = {'features': np.array(car_images), 'labels': np.array(steering_angles)}
 
